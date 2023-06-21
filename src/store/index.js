9012ed81e921c6b5c33a9d53399915c5a61f9d2b
Daria Vikhoreva <darja.vihoreva@yandex.ru>
2023-06-21 23:25:26 +0400

    update Vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -9,15 +9,34 @@ const store = new Vuex.Store({
 		cards: [],
 	},
 	mutations: {
-		createNewTask: function (state, card) {
-			state.cards.push(card);
+		ADD_ITEM: function (state, payload) {
+			state.cards.push(payload);
 		},
-		deleteTask: function (state, index) {
+		DELETE_ITEM: function (state, payload) {
+			const index = state.cards.indexOf(payload);
 			state.cards.splice(index, 1);
 		},
+		EDIT_ITEM: function (state, payload) {
+			const card = state.cards.find((task) => task.id === payload.id);
+			card.title = payload.newTitle;
+			card.description = payload.newDescription;
+		},
 	},
-	actions: {},
-	getters: {},
+	actions: {
+		ADD_TASK: function (context, payload) {
+			context.commit('ADD_ITEM', payload);
+		},
+		DELETE_TASK: function (context, payload) {
+			context.commit('DELETE_ITEM', payload);
+		},
+		EDIT_TASK: function (context, payload) {
+			context.commit('EDIT_ITEM', payload);
+		},
+	},
+	getters: {
+		getIDs: (state) => state.cards.map(({ id }) => id),
+		getTasks: (state) => state.cards,
+	},
 	plugins: [createPersistedState()],
 });
 export default store;
